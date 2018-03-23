ee5b2f84c55d0ca5ca2b7e7c76d918b6c0d03926
FattyMieo <fong2009@live.com.my>
2018-03-23 13:36:41 +0800

    Finish Chess Board

---
+++ Assets/MaterialPainter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MaterialPainter : MonoBehaviour
+{
+	public Material mat;
+	public MeshRenderer[] rends;
+
+	[ContextMenu("Update Material")]
+	public void UpdateMaterial ()
+	{
+		for(int i = 0; i < rends.Length; i++)
+		{
+			rends[i].material = mat;
+		}
+	}
+}
